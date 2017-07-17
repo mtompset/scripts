445025359c54d06ca0c1a7592138787bd037db0b
Mark Tompsett <mtompset@hotmail.com>
2017-07-17 11:43:40 -0400

    Improved input/output

--- resort.pl
+++ resort.pl
@@ -15,90 +15,129 @@
 # You should have received a copy of the GNU General Public License
 # along with this; if not, see <http://www.gnu.org/licenses>.
 
-use Modern::Perl;
-use Getopt::Long;
 use Const::Fast;
 use Carp;
+use English qw( -no_match_vars );
+use Getopt::Long;
+use Modern::Perl;
 
-const my $TagSize => 3;
+const my $TAGSIZE              => 3;
+const my $IND1_LOCATION        => 6;
+const my $IND2_LOCATION        => 7;
+const my $SUBFIELDS_START      => 8;
+const my $NONCONTROL_TAG_START => 10;
 
-my $InputFile  = 'input.mrk';
-my $OutputFile = 'sorted.mrk';
-my $KeyString  = '100,245,264,300,490';
-my @KeyFields;
+my $input_file  = 'input.mrk';
+my $output_file = 'sorted.mrk';
+my $key_string  = q{100$a,245$a,264$a/260$a,300$a,490$a,700$a};
+my @key_fields;
 
-GetOptions ("input|i=s"  => \$InputFile,
-            "output|o=s" => \$OutputFile,
-            "key|k=s"    => \$KeyString,
-           );
+GetOptions(
+    'input|i=s'  => \$input_file,
+    'output|o=s' => \$output_file,
+    'key|k=s'    => \$key_string,
+);
 
-@KeyFields = split(/,/xsm,$KeyString);
+@key_fields = split /,/xsm, $key_string;
 
 my $fh;
 my @data;
-if (open $fh, "<:encoding(UTF-8)", $InputFile) {
-    @data = <$fh>;
-    close $fh;
-}
-else {
-    carp "Can not open $InputFile: $!";
+if ( !open $fh, '<:encoding(UTF-8)', $input_file ) {
+    carp "Can not open $input_file: $ERRNO";
 }
 
+@data = <$fh>;
+close $fh;
+
 my @records;
-my $RecordData = q{};
+my $record_data = q{};
 foreach my $line (@data) {
     $line =~ s/^\s*//xsm;
     $line =~ s/\s*$//xsm;
-    if ($line eq {}) {
-        if ($RecordData) { chop $RecordData; } # this is a windows file.
-        if ($RecordData) { chop $RecordData; }
-        if ($RecordData) { push @records, $RecordData; }
-        $RecordData = q{};
+    if ( $line eq q{} ) {
+        if ($record_data) { chop $record_data; }    # this is a windows file.
+        if ($record_data) { chop $record_data; }
+        if ($record_data) { push @records, $record_data; }
+        $record_data = q{};
     }
     else {
-        $RecordData .= $line . "\r\n";
+        $record_data .= $line . "\r\n";
     }
 }
-if ($RecordData) {
-    if ($RecordData) { chop $RecordData; } # this is a windows file.
-    if ($RecordData) { chop $RecordData; }
-    if ($RecordData) { push @records, $RecordData; }
-    $RecordData = q{};
+if ($record_data) {
+    if ($record_data) { chop $record_data; }           # this is a windows file.
+    if ($record_data) { chop $record_data; }
+    if ($record_data) { push @records, $record_data; }
 }
 
-my @sorted = sort { determine_key($a,\@KeyFields) cmp
-                    determine_key($b,\@KeyFields) } @records;
+my @sorted =
+  sort { build_hash( $a, \@key_fields ) cmp build_hash( $b, \@key_fields ) }
+  @records;
 
-if (open $fh, ">:encoding(UTF-8)", $OutputFile) {
-    foreach my $RecordData (@sorted) {
-        print $fh $RecordData . "\r\n\r\n";
-    }
-    close $fh;
-}
-else {
-    carp "Can't make $OutputFile: $!";
+if ( !open $fh, '>:encoding(UTF-8)', $output_file ) {
+    carp "Can't make $output_file: $ERRNO";
 }
 
-sub determine_key {
-    my ($tRecordData,$keyfields) = @_;
+foreach my $sorted_record (@sorted) {
+    my $line = $sorted_record . "\r\n\r\n";
+    print {$fh} $line;
+}
+close $fh;
 
-    my @Fields2Check = @{$keyfields};
-    my @fields       = split(/\n/xsm,$tRecordData);
-    my %KeyPieces;
-    my $key;
-    foreach my $tag (@Fields2Check) {
-        $KeyPieces{$tag} = q{};
-    }
-    foreach my $field (sort @fields) {
-        my $tag = substr($field,1,$TagSize);
-        my @Matches = grep { $_ eq $tag } @Fields2Check;
-        if (@Matches>0) {
-            $KeyPieces{$tag} = $field;
+sub build_hash {
+    my ( $current_record, $keyfields ) = @_;
+
+    my @fields = split /\r\n/, $current_record;
+    my $hashed_data;
+    foreach my $field (@fields) {
+        my $tag = substr $field, 1, $TAGSIZE;
+        my $ind1 = substr $field, $IND1_LOCATION, 1;
+        my $ind2 = substr $field, $IND2_LOCATION, 1;
+        my $subfields = substr $field, $SUBFIELDS_START;
+        $hashed_data->{$tag}->{'ind1'} = $ind1;
+        $hashed_data->{$tag}->{'ind2'} = $ind2;
+        if ( $tag eq 'LDR' || int($tag) < $NONCONTROL_TAG_START ) {
+            $hashed_data->{$tag}->{q{@}} = $subfields;
+        }
+        else {
+            $subfields = substr $subfields, 1;    # ignore first $
+            my @subfield_data = split /\$/, $subfields;
+            my @subfield_hashes = map {
+                { 'subtag' => substr( $_, 0, 1 ), 'value' => substr $_, 1 }
+            } @subfield_data;
+            foreach my $subfield_hash (@subfield_hashes) {
+                my $subtag = $subfield_hash->{'subtag'};
+                my $value  = $subfield_hash->{'value'};
+                $hashed_data->{$tag}->{$subtag} = $value;
+            }
         }
     }
-    $key = q{};
-    foreach my $tag (sort keys %KeyPieces) {
-        $key .= $KeyPieces{$tag};
+    my @key_values;
+    foreach my $key_piece ( @{$keyfields} ) {
+
+        # No multiple conditions to build.
+        if ( $key_piece !~ /\// ) {
+            my ( $keytag, $keysubtag ) = split /\$/, $key_piece;
+            my $key_value = $hashed_data->{$keytag}->{$keysubtag} // q{};
+            push @key_values, $key_value;
+        }
+        else {
+            my $part_found = 0;
+            my @conditions = split /\//, $key_piece;
+            foreach my $condition (@conditions) {
+                my ( $keytag, $keysubtag ) = split /\$/, $key_piece;
+                my $key_value = $hashed_data->{$keytag}->{$keysubtag};
+                if ( defined $key_value ) {
+                    push @key_values, $key_value;
+                    $part_found = 1;
+                    break;
+                }
+            }
+            if ( $part_found == 0 ) {
+                push @key_values, q{};
+            }
+        }
     }
+    my $key = join q{}, @key_values;
     return $key;
 }
